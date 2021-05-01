0ca515e54a22b5e87a9d08c452684df371e284c7
julien.wut <julien.wut@gmail.com>
2021-05-01 00:03:41 -0700

    update prediction route

--- smart_compose/api/api/routes/prediction.py
+++ smart_compose/api/api/routes/prediction.py
@@ -1,6 +1,7 @@
 from starlette.requests import Request
 from fastapi import APIRouter, Depends
 
+from smart_compose.api.core.security import *
 from smart_compose.api.services.models import SentimentModel
 from smart_compose.api.models.payload import SentimentPayload
 from smart_compose.api.models.prediction import SentimentPredictionResult
@@ -8,18 +9,14 @@ from smart_compose.api.models.prediction import SentimentPredictionResult
 router = APIRouter()
 
 
-@router.post("/predict_sentiment", response_model=SentimentPayload,
-             name="predict_sentiment")
+@router.post("/predict_sentiment", response_model=SentimentPayload, name="predict_sentiment")
 def post_predict(
     request: Request,
     authenticated: bool = Depends(validate_request),
     block_data: SentimentPayload = None
 ) -> SentimentPredictionResult:
     if not authenticated:
-        raise HTTPException(
-            status_code=HTTP_401_UNAUTHORIZED,
-            detail=AUTH_REQ,
-            headers={})
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail=AUTH_REQ, headers={})
 
     model: SentimentModel = request.app.state.model
     prediction: SentimentPredictionResult = model.predict(block_data)
